a38484c56836b89881acf3f2edfea49a673018bc
lazar-malezanov <lazar.malezanov@abv.bg>
2017-08-08 12:28:56 +0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace DisgustingLittleSillyScaryDungeon
+{
+    partial class DisgustingLittleSillyScaryDungeon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::DisgustingLittleSillyScaryDungeon.Properties.Resources._2;
+            this.panel1.Controls.Add(this.player);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1080, 704);
+            this.panel1.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.Image = global::DisgustingLittleSillyScaryDungeon.Properties.Resources._3;
+            this.player.Location = new System.Drawing.Point(0, 649);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(54, 54);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // DisgustingLittleSillyScaryDungeon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 704);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DisgustingLittleSillyScaryDungeon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Disgusting Little Silly Scary Dungeon";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DisgustingLittleSillyScaryDungeon_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.DisgustingLittleSillyScaryDungeon_KeyUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisgustingLittleSillyScaryDungeon
+{
+    public partial class DisgustingLittleSillyScaryDungeon : Form
+    {
+        bool right;
+        bool left;
+        bool up;
+        bool down;
+
+        public DisgustingLittleSillyScaryDungeon()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.right == true)
+            {
+                player.Left += 54;
+            }
+
+            if (this.left == true)
+            {
+                player.Left -= 54;
+            }
+
+            if (this.up == true)
+            {
+                player.Top -= 54;
+            }
+
+            if (this.down == true)
+            {
+                player.Top += 54;
+            }
+        }
+
+        private void DisgustingLittleSillyScaryDungeon_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                this.right = true;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                this.left = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                this.up = true;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                this.down = true;
+            }
+        }
+
+        private void DisgustingLittleSillyScaryDungeon_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                this.right = false;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                this.left = false;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                this.up = false;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                this.down = false;
+            }
+        }
+    }
+}
