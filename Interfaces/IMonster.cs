a6279a9af2dff0ac0f1fb4df2b84457a557afa77
Dimitar Yordanov <dimitar.yordanov.12@gmail.com>
2017-08-09 17:43:49 +0300

    Adding Interfaces

---
+++ Interfaces/IMonster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroupProject.Positioning;
+using GroupProject.Common.Enums;
+
+namespace GroupProject.Interfaces
+{
+    public interface IMonster
+    {
+        Position Position { get; set; }
+        int Health { get; set; }
+        int MaxDamage { get; set; }
+        int Defence { get; set; }
+        string Battlecry { get; set; }
+
+        void Attack();
+
+    }
+}
